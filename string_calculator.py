35117f02b093b35561c4310ce916a20df68d26fe
Ayush Shah <ayushshahh4403@gmail.com>
2025-07-04 22:42:22 +0530

    Return sum of two comma-separated numbers

--- string_calculator.py
+++ string_calculator.py
@@ -1,5 +1,8 @@
 def add(numbers):
     if numbers == "":
         return 0
+    if "," in numbers:
+        nums = numbers.split(",")
+        return int(nums[0]) + int(nums[1])
     return int(numbers)
 
--- test.py
+++ test.py
@@ -4,8 +4,12 @@ from string_calculator import add
 class TestStringCalculator(unittest.TestCase):
     def test_empty_string(self):
         self.assertEqual(add(""), 0)
+        
     def test_single_number(self):
         self.assertEqual(add("6"), 6)
+        
+    def test_two_numbers_comma_seperated(self):
+        self.assertEqual(add("5,7"), 12)
         
 if __name__ == "__main__":
     unittest.main()
